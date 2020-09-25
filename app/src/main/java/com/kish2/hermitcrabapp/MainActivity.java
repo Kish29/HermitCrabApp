2835337231ac096ba2e46666337325c1579e7149
Kish29 <875691208@qq.com>
2020-09-26 00:36:18 +0800

    initialize repository

---
+++ app/src/main/java/com/kish2/hermitcrabapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kish2.hermitcrabapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
